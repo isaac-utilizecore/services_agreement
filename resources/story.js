8e365f8eb2b9eb36378b8a52264fadf07ad274be
isaac-utilizecore <43425407+isaac-utilizecore@users.noreply.github.com>
2019-02-13 23:58:15 -0500

    Add files via upload

---
+++ resources/story.js
@@ -0,0 +1,1274 @@
+var story = {
+"docName": "services-agreement",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"pages": [
+{
+"image": "site_view.png",
+"width": 1455,
+"height": 1338,
+"title": "site_view",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    8,
+    954,
+    170,
+    1011
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    8,
+    954,
+    170,
+    1011
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    1331,
+    1212,
+    1351,
+    1232
+   ],
+   "page": 35
+  }
+,{
+  "rect": [
+    1331,
+    1212,
+    1351,
+    1232
+   ],
+   "page": 35
+  }
+]}
+,{
+"image": "services_agreement_1.png",
+"width": 1138,
+"height": 734,
+"title": "services_agreement_1",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    930,
+    578,
+    1041,
+    635
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    930,
+    578,
+    1041,
+    635
+   ],
+   "page": 2
+  }
+]}
+,{
+"image": "services_agreement_2.png",
+"width": 1140,
+"height": 558,
+"title": "services_agreement_2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    568,
+    138,
+    1032,
+    417
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    568,
+    138,
+    1032,
+    417
+   ],
+   "page": 3
+  }
+]}
+,{
+"image": "services_agreement_3.png",
+"width": 1138,
+"height": 554,
+"title": "services_agreement_3",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    113,
+    135,
+    567,
+    414
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    113,
+    135,
+    567,
+    414
+   ],
+   "page": 4
+  }
+]}
+,{
+"image": "services_agreement_4.png",
+"width": 1138,
+"height": 821,
+"title": "services_agreement_4",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    930,
+    664,
+    1041,
+    721
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    930,
+    664,
+    1041,
+    721
+   ],
+   "page": 5
+  }
+]}
+,{
+"image": "services_agreement_5.png",
+"width": 1138,
+"height": 1201,
+"title": "services_agreement_5",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    930,
+    1043,
+    1041,
+    1100
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    930,
+    1043,
+    1041,
+    1100
+   ],
+   "page": 6
+  }
+]}
+,{
+"image": "services_agreement_6.png",
+"width": 1138,
+"height": 827,
+"title": "services_agreement_6",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    933,
+    672,
+    1044,
+    729
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    933,
+    672,
+    1044,
+    729
+   ],
+   "page": 7
+  }
+]}
+,{
+"image": "services_agreement_7_1.png",
+"width": 1140,
+"height": 994,
+"title": "services_agreement_7_1",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    116,
+    454,
+    513,
+    519
+   ],
+   "page": 8
+  }
+,{
+  "rect": [
+    116,
+    454,
+    513,
+    519
+   ],
+   "page": 8
+  }
+]}
+,{
+"image": "services_agreement_7_2.png",
+"width": 1140,
+"height": 994,
+"title": "services_agreement_7_2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    929,
+    469,
+    997,
+    505
+   ],
+   "page": 9
+  }
+,{
+  "rect": [
+    929,
+    469,
+    997,
+    505
+   ],
+   "page": 9
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_1_1.png",
+"width": 1138,
+"height": 631,
+"title": "services_agreement_vendor_setup_1_1",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    266,
+    280,
+    542,
+    428
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    266,
+    280,
+    542,
+    428
+   ],
+   "page": 10
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_2_1.png",
+"width": 1140,
+"height": 994,
+"title": "services_agreement_vendor_setup_2_1",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    487,
+    663,
+    612,
+    704
+   ],
+   "page": 11
+  }
+,{
+  "rect": [
+    487,
+    663,
+    612,
+    704
+   ],
+   "page": 11
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_services_1_1_1.png",
+"width": 740,
+"height": 650,
+"title": "services_agreement_vendor_setup_services_1_1_1",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    294,
+    497,
+    325,
+    530
+   ],
+   "page": 12
+  }
+,{
+  "rect": [
+    294,
+    497,
+    325,
+    530
+   ],
+   "page": 12
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_services_1_2_1.png",
+"width": 740,
+"height": 650,
+"title": "services_agreement_vendor_setup_services_1_2_1",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    580,
+    567,
+    715,
+    632
+   ],
+   "page": 13
+  }
+,{
+  "rect": [
+    580,
+    567,
+    715,
+    632
+   ],
+   "page": 13
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_services_1_3_1.png",
+"width": 740,
+"height": 650,
+"title": "services_agreement_vendor_setup_services_1_3_1",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    580,
+    567,
+    715,
+    632
+   ],
+   "page": 14
+  }
+,{
+  "rect": [
+    580,
+    567,
+    715,
+    632
+   ],
+   "page": 14
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_services_1_4.png",
+"width": 740,
+"height": 595,
+"title": "services_agreement_vendor_setup_services_1_4",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    577,
+    512,
+    712,
+    577
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    577,
+    512,
+    712,
+    577
+   ],
+   "page": 15
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_2_2.png",
+"width": 1140,
+"height": 994,
+"title": "services_agreement_vendor_setup_2_2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    660,
+    406,
+    683,
+    463
+   ],
+   "page": 16
+  }
+,{
+  "rect": [
+    660,
+    406,
+    683,
+    463
+   ],
+   "page": 16
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_services_1_1.png",
+"width": 893,
+"height": 1442,
+"title": "services_agreement_vendor_setup_services_1_1",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    506,
+    256,
+    702,
+    1290
+   ],
+   "page": 17
+  }
+,{
+  "rect": [
+    506,
+    256,
+    702,
+    1290
+   ],
+   "page": 17
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_services_1_2.png",
+"width": 893,
+"height": 1442,
+"title": "services_agreement_vendor_setup_services_1_2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    650,
+    1352,
+    807,
+    1421
+   ],
+   "page": 18
+  }
+,{
+  "rect": [
+    650,
+    1352,
+    807,
+    1421
+   ],
+   "page": 18
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_2_3.png",
+"width": 1140,
+"height": 994,
+"title": "services_agreement_vendor_setup_2_3",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    939,
+    406,
+    962,
+    463
+   ],
+   "page": 19
+  }
+,{
+  "rect": [
+    939,
+    406,
+    962,
+    463
+   ],
+   "page": 19
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_services_2_1.png",
+"width": 893,
+"height": 1442,
+"title": "services_agreement_vendor_setup_services_2_1",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    506,
+    256,
+    702,
+    1290
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    506,
+    256,
+    702,
+    1290
+   ],
+   "page": 20
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_services_2_2.png",
+"width": 893,
+"height": 1442,
+"title": "services_agreement_vendor_setup_services_2_2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    644,
+    1353,
+    815,
+    1419
+   ],
+   "page": 21
+  }
+,{
+  "rect": [
+    644,
+    1353,
+    815,
+    1419
+   ],
+   "page": 21
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_2_4.png",
+"width": 1140,
+"height": 994,
+"title": "services_agreement_vendor_setup_2_4",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    995,
+    405,
+    1012,
+    462
+   ],
+   "page": 22
+  }
+,{
+  "rect": [
+    995,
+    405,
+    1012,
+    462
+   ],
+   "page": 22
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_services_1_3.png",
+"width": 900,
+"height": 300,
+"title": "services_agreement_vendor_setup_services_1_3",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    627,
+    144,
+    739,
+    193
+   ],
+   "page": 23
+  }
+,{
+  "rect": [
+    627,
+    144,
+    739,
+    193
+   ],
+   "page": 23
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_2_5.png",
+"width": 1140,
+"height": 994,
+"title": "services_agreement_vendor_setup_2_5",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    909,
+    831,
+    1016,
+    888
+   ],
+   "page": 24
+  }
+,{
+  "rect": [
+    909,
+    831,
+    1016,
+    888
+   ],
+   "page": 24
+  }
+]}
+,{
+"image": "services_agreement_7_3.png",
+"width": 1140,
+"height": 994,
+"title": "services_agreement_7_3",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    116,
+    454,
+    513,
+    519
+   ],
+   "page": 25
+  }
+,{
+  "rect": [
+    116,
+    454,
+    513,
+    519
+   ],
+   "page": 25
+  }
+]}
+,{
+"image": "services_agreement_7_4.png",
+"width": 1140,
+"height": 994,
+"title": "services_agreement_7_4",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    929,
+    469,
+    997,
+    505
+   ],
+   "page": 26
+  }
+,{
+  "rect": [
+    929,
+    469,
+    997,
+    505
+   ],
+   "page": 26
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_1_2.png",
+"width": 1138,
+"height": 631,
+"title": "services_agreement_vendor_setup_1_2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    581,
+    280,
+    857,
+    428
+   ],
+   "page": 27
+  }
+,{
+  "rect": [
+    581,
+    280,
+    857,
+    428
+   ],
+   "page": 27
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_2_6.png",
+"width": 1140,
+"height": 994,
+"title": "services_agreement_vendor_setup_2_6",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    487,
+    663,
+    612,
+    704
+   ],
+   "page": 28
+  }
+,{
+  "rect": [
+    487,
+    663,
+    612,
+    704
+   ],
+   "page": 28
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_services_1_1_2.png",
+"width": 740,
+"height": 650,
+"title": "services_agreement_vendor_setup_services_1_1_2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    580,
+    567,
+    715,
+    632
+   ],
+   "page": 29
+  }
+,{
+  "rect": [
+    580,
+    567,
+    715,
+    632
+   ],
+   "page": 29
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_services_1_2_2.png",
+"width": 740,
+"height": 650,
+"title": "services_agreement_vendor_setup_services_1_2_2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    580,
+    567,
+    715,
+    632
+   ],
+   "page": 30
+  }
+,{
+  "rect": [
+    580,
+    567,
+    715,
+    632
+   ],
+   "page": 30
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_services_1_3_2.png",
+"width": 740,
+"height": 595,
+"title": "services_agreement_vendor_setup_services_1_3_2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    580,
+    514,
+    715,
+    579
+   ],
+   "page": 31
+  }
+,{
+  "rect": [
+    580,
+    514,
+    715,
+    579
+   ],
+   "page": 31
+  }
+]}
+,{
+"image": "services_agreement_vendor_setup_2_7.png",
+"width": 1140,
+"height": 994,
+"title": "services_agreement_vendor_setup_2_7",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    906,
+    829,
+    1017,
+    886
+   ],
+   "page": 32
+  }
+,{
+  "rect": [
+    906,
+    829,
+    1017,
+    886
+   ],
+   "page": 32
+  }
+]}
+,{
+"image": "services_agreement_7_5.png",
+"width": 1140,
+"height": 994,
+"title": "services_agreement_7_5",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    906,
+    829,
+    1017,
+    886
+   ],
+   "page": 33
+  }
+,{
+  "rect": [
+    906,
+    829,
+    1017,
+    886
+   ],
+   "page": 33
+  }
+]}
+,{
+"image": "services_agreement_8.png",
+"width": 1138,
+"height": 493,
+"title": "services_agreement_8",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    937,
+    338,
+    1037,
+    389
+   ],
+   "page": 34
+  }
+,{
+  "rect": [
+    937,
+    338,
+    1037,
+    389
+   ],
+   "page": 34
+  }
+,{
+  "rect": [
+    845,
+    138,
+    1025,
+    179
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    845,
+    138,
+    1025,
+    179
+   ],
+   "page": 4
+  }
+]}
+,{
+"image": "services_agreement_9.png",
+"width": 1138,
+"height": 735,
+"title": "services_agreement_9",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    955,
+    676,
+    1055,
+    727
+   ],
+   "page": 35
+  }
+,{
+  "rect": [
+    955,
+    676,
+    1055,
+    727
+   ],
+   "page": 35
+  }
+,{
+  "rect": [
+    822,
+    152,
+    1023,
+    193
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    822,
+    152,
+    1023,
+    193
+   ],
+   "page": 4
+  }
+]}
+,{
+"image": "services_agreement_viewpage_1.png",
+"width": 1440,
+"height": 900,
+"title": "services_agreement_viewpage_1",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1414,
+    656,
+    1433,
+    675
+   ],
+   "page": 36
+  }
+,{
+  "rect": [
+    1414,
+    656,
+    1433,
+    675
+   ],
+   "page": 36
+  }
+,{
+  "rect": [
+    1268,
+    327,
+    1287,
+    346
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    1268,
+    327,
+    1287,
+    346
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    675,
+    646,
+    731,
+    702
+   ],
+   "page": 0
+  }
+,{
+  "rect": [
+    675,
+    646,
+    731,
+    702
+   ],
+   "page": 0
+  }
+,{
+  "rect": [
+    251,
+    507,
+    348,
+    539
+   ],
+   "page": 37
+  }
+,{
+  "rect": [
+    251,
+    507,
+    348,
+    539
+   ],
+   "page": 37
+  }
+,{
+  "rect": [
+    361,
+    507,
+    541,
+    539
+   ],
+   "page": 39
+  }
+,{
+  "rect": [
+    361,
+    507,
+    541,
+    539
+   ],
+   "page": 39
+  }
+]}
+,{
+"image": "services_agreement_workorderview_1.png",
+"width": 1440,
+"height": 2945,
+"title": "services_agreement_workorderview_1",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    826,
+    1381,
+    857,
+    1412
+   ],
+   "page": 40
+  }
+,{
+  "rect": [
+    826,
+    1381,
+    857,
+    1412
+   ],
+   "page": 40
+  }
+,{
+  "rect": [
+    1202,
+    2859,
+    1221,
+    2878
+   ],
+   "page": 35
+  }
+,{
+  "rect": [
+    1202,
+    2859,
+    1221,
+    2878
+   ],
+   "page": 35
+  }
+]}
+,{
+"image": "services_agreement_viewpage_2.png",
+"width": 1440,
+"height": 900,
+"title": "services_agreement_viewpage_2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1327,
+    723,
+    1346,
+    742
+   ],
+   "page": 38
+  }
+,{
+  "rect": [
+    1327,
+    723,
+    1346,
+    742
+   ],
+   "page": 38
+  }
+,{
+  "rect": [
+    361,
+    507,
+    541,
+    539
+   ],
+   "page": 39
+  }
+,{
+  "rect": [
+    361,
+    507,
+    541,
+    539
+   ],
+   "page": 39
+  }
+,{
+  "rect": [
+    17,
+    507,
+    158,
+    539
+   ],
+   "page": 35
+  }
+,{
+  "rect": [
+    17,
+    507,
+    158,
+    539
+   ],
+   "page": 35
+  }
+]}
+,{
+"image": "services_agreement_invoice.png",
+"width": 631,
+"height": 825,
+"title": "services_agreement_invoice",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    30,
+    24,
+    87,
+    56
+   ],
+   "action": "back"
+  }
+]}
+,{
+"image": "services_agreement_viewpage_3.png",
+"width": 1440,
+"height": 900,
+"title": "services_agreement_viewpage_3",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    17,
+    507,
+    158,
+    539
+   ],
+   "page": 35
+  }
+,{
+  "rect": [
+    17,
+    507,
+    158,
+    539
+   ],
+   "page": 35
+  }
+,{
+  "rect": [
+    1338,
+    733,
+    1357,
+    752
+   ],
+   "page": 40
+  }
+,{
+  "rect": [
+    1338,
+    733,
+    1357,
+    752
+   ],
+   "page": 40
+  }
+,{
+  "rect": [
+    251,
+    507,
+    348,
+    539
+   ],
+   "page": 37
+  }
+,{
+  "rect": [
+    251,
+    507,
+    348,
+    539
+   ],
+   "page": 37
+  }
+]}
+,{
+"image": "services_agreement_po.png",
+"width": 631,
+"height": 825,
+"title": "services_agreement_po",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    30,
+    24,
+    87,
+    56
+   ],
+   "action": "back"
+  }
+]}
+   ]
+,"resolutions": [1],
+"title": "services_agreement",
+"highlightLinks": false
+}
